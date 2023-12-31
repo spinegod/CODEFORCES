439e7f2e3c3906a55639ee1e6a7ef39805eeb59d
SIDDHARTH SINGH <98952712+spinegod@users.noreply.github.com>
2023-12-31 19:13:27 +0530

    Add files via upload

---
+++ 1335A.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <math.h>
+#include <limits.h>
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        ll cnt = (n-1)/2;
+        cout<<cnt<<endl;
+    }
+}
