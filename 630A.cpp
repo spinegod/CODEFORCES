6c70184138d4d57eac682b3ba17c8cebfe26d130
SIDDHARTH SINGH <98952712+spinegod@users.noreply.github.com>
2024-01-02 19:34:30 +0530

    Add files via upload

---
+++ 630A.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <math.h>
+#include <limits.h>
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    ll n;
+    cin>>n;
+    cout<<25;
+}
